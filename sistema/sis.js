43a03c7e90f173d0037ca5025b1c6315ff953fbe
igorjaime09 <igorjaime459@gmail.com>
2023-11-03 23:51:48 -0300

    2.0

--- sistema/sis.js
+++ sistema/sis.js
@@ -65,6 +65,85 @@ window.addEventListener('resize', function () {
 	}
 })
 
+document.addEventListener('DOMContentLoaded', function() {
+	// Recupere a imagem base64 do localStorage
+	const base64String = localStorage.getItem('profilePicBase64');
+	
+	// Verifique se a imagem existe no localStorage
+	if (base64String) {
+		// Encontre o elemento de imagem na segunda página e defina a imagem
+		document.getElementById('profile-pic').src = base64String;
+	}
+	});
+
+let inputFile = document.getElementById('input-file');
+
+	inputFile.onchange = function() {
+		const file = inputFile.files[0];
+		const reader = new FileReader();
+
+		reader.onload = function() {
+			const base64String = reader.result;
+			// Salvar a imagem base64 no localStorage
+			localStorage.setItem('profilePicBase64', base64String);
+
+
+			document.getElementById('profile-pic').src = base64String;
+		};
+
+		reader.readAsDataURL(file);
+
+		// Lê e salva a segunda imagem base64
+		const file2 = inputFile.files[0];
+		const reader2 = new FileReader();
+
+		reader2.onload = function() {
+			const base64String2 = reader2.result;
+
+			// Salve a segunda imagem base64 no localStorage
+			localStorage.setItem('profilePicBase64Copy', base64String2);
+
+			// Defina a imagem carregada como a nova imagem de perfil para o segundo elemento
+			document.getElementById('profile-pic2').src = base64String2;
+		};
+
+		if (file2) {
+			reader2.readAsDataURL(file2);
+		}
+	};
+
+	let profilePic = document.getElementById('profile-pic');
+
+
+	const base64String = localStorage.getItem('profilePicBase64');
+
+	if (base64String) {
+
+
+	profilePic.src = base64String;
+	} else {
+	
+
+	profilePic.src = '../assets/profile.png';
+	}
+
+	let profilePic2 = document.getElementById('profile-pic2');
+
+
+	const base64StringCopy = localStorage.getItem('profilePicBase64Copy');
+
+	if (base64StringCopy) {
+
+
+	profilePic2.src = base64StringCopy;
+	} else {
+	
+
+	profilePic2.src = '../assets/profile.png';
+	}
+
+	
+
 
 
 
